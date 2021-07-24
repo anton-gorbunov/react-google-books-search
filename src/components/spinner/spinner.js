d078f16ec63cd73b304889a5a3f78bbb04939d9f
Anton <gorbunov.anton02@yandex.ru>
2021-07-24 23:23:14 +0300

    add logic

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './spinner.scss';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-eclipse-2vh6qt9nilo">
+            <div className="ldio-xnbfxxwq1da">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner;
---
+++ src/libs/functions.js
@@ -0,0 +1,65 @@
+const isSet = (data) => {
+    if (data) {
+        return data;
+    } else {
+        return '';
+    }
+}
+const isImage = (img) => {
+    if (img === undefined) {
+        return 'https://i.pinimg.com/736x/c0/ce/2f/c0ce2fe2c02313f47c0757a7be80ed6e.jpg';
+    } else {
+        return img.thumbnail
+    }
+}
+const getStrFromArr = (arr) => {
+    if(arr === undefined) {
+        return '';
+    } else if (arr instanceof Array){
+        return arr.join(', ')
+    } else {
+        return arr;
+    }
+}
+const getFirstCategory = (categories) => {
+    if(categories === undefined) {
+        return '';
+    } else if (categories instanceof Array) {
+        return categories[0];
+    } else {
+        return categories;
+    }
+}
+const changeDate = (date) => {
+    if (date === undefined) {
+        return '';
+    } else {
+        return date.split('-').reverse().join('.');
+    }
+}
+const getShortTitle = (str) => {
+    if (str.length > 70) {
+        return `${str.substr(0,100)}...`;
+    } else {
+        return str;
+    }
+}
+const transformBooks = (book) => {
+    return {
+        id:book.id,
+        category: isSet(book.volumeInfo.categories),
+        title:isSet(book.volumeInfo.title),
+        language:isSet(book.volumeInfo.language),
+        authors:getStrFromArr(book.volumeInfo.authors),
+        publishedDate:changeDate(book.volumeInfo.publishedDate),
+        descr:isSet(book.volumeInfo.description),
+        img:isImage(book.volumeInfo.imageLinks)
+    }
+}
+
+export {
+    transformBooks,
+    getFirstCategory,
+    getStrFromArr,
+    getShortTitle
+}
